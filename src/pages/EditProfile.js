3a9bf6c3e8cd47662ba2a28cc5d451f75641d497
Ayushgupta18 <itsayushgupta18@gmail.com>
2022-10-31 00:09:18 +0530

    feat: integrate api for edit profile

--- src/pages/EditProfile.js
+++ src/pages/EditProfile.js
@@ -1,38 +1,64 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useMoralis } from "react-moralis";
 import moneyImage from "../assets/images/money.png"
 
 function EditProfile() {
+  const { user } = useMoralis();
   const [editProfileForm, setEditProfileForm] = useState({
-    userName: '',
+    username: '',
     displayName: '',
     bio: '',
-    links: {
-      spotify: '',
-      youtube: '',
-      twitter: '',
-      instagram: ''
-    }
+    spotify: '',
+    youtube: '',
+    twitter: '',
+    instagram: ''
   });
+
+  const handleOnChange = (e) => {
+    setEditProfileForm({ ...editProfileForm, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = () => {
+    user.save(editProfileForm);
+  }
+
+  useEffect(() => {
+    if (user)
+      fetchUserProfile()
+  }, [user]);
+
+  const fetchUserProfile = async () => {
+    const { username, bio, spotify, youtube, twitter, instagram } = await user.attributes
+    setEditProfileForm({
+      ...editProfileForm,
+      username: username ?? '',
+      bio: bio ?? '',
+      spotify: spotify ?? '',
+      youtube: youtube ?? '',
+      twitter: twitter ?? '',
+      instagram: instagram ?? ''
+    })
+  }
 
   return (
     <div className="w-1/2 my-32">
       <p className="text-5xl font-semibold">Edit Profile</p>
       <div className="my-16 flex items-center"><img src={moneyImage} alt="Money" className="rounded-full" /> <button className="rounded-full px-4 py-2 border ml-4">Upload New</button></div>
 
-      <label for="userName">
+      <label for="username">
         *Username
       </label>
-      <input className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" id="userName" placeholder="username" name="userName" value={editProfileForm.userName} />
+      <input onChange={handleOnChange} className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" id="username" placeholder="username" name="username" value={editProfileForm.username} />
 
       <label for="displayName">
         *Display Name
       </label>
-      <input className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="Your real name (or whatever name you go by)" id="displayName" name="displayName" value={editProfileForm.displayName} />
+      <input onChange={handleOnChange} className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="Your real name (or whatever name you go by)" id="displayName" name="displayName" value={editProfileForm.displayName} />
 
       <label for="bio">
         Share a short bio
       </label>
-      <textarea className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="What music you’re into, what music you make, etc..." id="bio" name="bio" value={editProfileForm.bio} />
+      <textarea onChange={handleOnChange} className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="What music you’re into, what music you make, etc..." id="bio" name="bio" value={editProfileForm.bio} />
 
       <hr className="mb-16 border border-gray-500" />
       <p className="text-3xl font-semibold mb-8">Include links to your social accounts:</p>
@@ -40,23 +66,24 @@ function EditProfile() {
       <label for="spotify">
         Spotify
       </label>
-      <input className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="spotify" id="spotify" name="spotify" value={editProfileForm.links.spotify} />
+      <input onChange={handleOnChange} className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="spotify" id="spotify" name="spotify" value={editProfileForm.spotify} />
 
       <label for="youtube">
         YouTube
       </label>
-      <input className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="youtube" id="youtube" name="youtube" value={editProfileForm.links.youtube} />
+      <input onChange={handleOnChange} className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="youtube" id="youtube" name="youtube" value={editProfileForm.youtube} />
 
       <label for="twitter">
         Twitter
       </label>
-      <input className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="twitter" id="twitter" name="twitter" value={editProfileForm.links.twitter} />
+      <input onChange={handleOnChange} className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="twitter" id="twitter" name="twitter" value={editProfileForm.twitter} />
 
-      <label for="userName">
+      <label for="username">
         Instagram
       </label>
-      <input className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="instagram" id='instagram' name="instagram" value={editProfileForm.links.instagram} />
+      <input onChange={handleOnChange} className="bg-[#3A3A40] rounded-md p-4 w-full mb-8" placeholder="instagram" id='instagram' name="instagram" value={editProfileForm.instagram} />
 
+      <button type="submit" onClick={handleSubmit} className="rounded-full bg-blue-500 px-4 py-4 disabled:opacity-75 mt-4">Save Changes</button>
     </div>
   );
 }
